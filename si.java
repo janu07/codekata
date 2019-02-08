3096d87878a4e5eb200533f51b798a0e8269cb87
janu07 <47377091+janu07@users.noreply.github.com>
2019-02-08 16:09:15 +0530

    Create si.java

---
+++ si.java
@@ -0,0 +1,17 @@
+import java.util.Scanner; 
+class si{
+
+    public static void main(String[] args) {
+                   Scanner ip=new Scanner(System.in);
+	int num=ip.nextInt();
+                    int count = 0;
+	
+        while(num != 0)
+        {
+            num = num/10;
+            ++count;
+        }
+
+        System.out.println("Number of digits: " + count);
+    }
+}
