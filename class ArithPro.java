250061c77ff40d67e7b7e6820aaccab99a24091c
janu07 <47377091+janu07@users.noreply.github.com>
2019-02-09 08:34:08 -0800

    Create class ArithPro.java

---
+++ class ArithPro.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class ArithPro {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        int n,a,d,val=0;
+		Scanner sc=new Scanner(System.in);
+        n=sc.nextInt();
+        a=sc.nextInt();
+        d=sc.nextInt();
+        val=(n*(2*a+(n-1)*d))/2;
+        System.out.println(val);
+        
+	}
+}
