d9e2a50a064205c5f9bce172e0b693a172627242
janu07 <47377091+janu07@users.noreply.github.com>
2019-02-17 02:26:35 -0800

    Create tim_min.java

---
+++ tim_min.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class tim_min
+{
+public static void main(String []agrs)
+{
+int hr,min;
+Scanner sj=new Scanner(System.in);
+int h1=sj.nextInt();
+int m1=sj.nextInt();
+int h2=sj.nextInt();
+int m2=sj.nextInt();
+hr=h2-h1;
+min=m2-m1;
+System.out.println(hr+" " +min);
+}
+}
