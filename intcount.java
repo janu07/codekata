d4a34f4e61c53b8306743792169e793ab72956ba
janu07 <47377091+janu07@users.noreply.github.com>
2019-02-17 03:42:05 -0800

    Create intcount.java

---
+++ intcount.java
@@ -0,0 +1,31 @@
+import java.util.*;
+import java.lang.*;
+class intcount
+{
+	public static void main(String args[])
+	{
+	      String a;
+	      int c=0;
+	      Scanner sj=new Scanner(System.in);
+	      a=sj.nextLine();
+	      for(int i=0;i<a.length();i++)
+	      {
+	          switch(a.charAt(i))
+	        {
+		case '0':
+		case '1':
+		case '2':
+		case '3': 
+		case '4':
+		case '5': 
+		case '6':
+		case '7':  
+		case '8':
+		case '	9': 
+			c++;
+			break;			
+	           }
+                         }
+	      System.out.println(c);
+    }    
+}
