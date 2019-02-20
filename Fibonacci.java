24a808301771a4bdf8440267360b6a6abaf57f7a
janu07 <47377091+janu07@users.noreply.github.com>
2019-02-20 16:32:58 +0530

    Update Fibonacci.java

--- Fibonacci.java
+++ Fibonacci.java
@@ -3,10 +3,10 @@ class Fibonacci
 {  
   public static void main(String args[])  
   {    
-      int n1=0,n2=1,n3,i;   
+      int n1=1,n2=1,n3,i;   
       Scanner sj=new Scanner(System.in);
       int count=sj.nextInt();
-      System.out.print(n1+" "+n2);//printing 0 and 1    
+      System.out.print(n1" "+n2);//printing 0 and 1    
     
       for(i=2;i<count;++i)//loop starts from 2 because 0 and 1 are already printed    
       {    
