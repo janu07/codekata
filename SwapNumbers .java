717684f147c159eda76a0ea1c72bbfb9c5b31bb1
janu07 <47377091+janu07@users.noreply.github.com>
2019-02-20 15:57:43 +0530

    Update SwapNumbers .java

--- SwapNumbers .java
+++ SwapNumbers .java
@@ -4,8 +4,6 @@ class SwapNumbers
 
     public static void main(String[] args) 
     {
-
-        float first = 12.0f, second = 24.5f;
         Scanner sj=new Scanner(System.in);
         int n1=sj.nextInt();
         int n2=sj.nextInt();
