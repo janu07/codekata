396df68b868d0ca1f0b259fe26d8dd81f0efc1fb
janu07 <47377091+janu07@users.noreply.github.com>
2019-02-23 02:57:35 -0800

    Create str_rep.java

---
+++ str_rep.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class str_rep
+{
+     public static void main(String aa[])
+     {
+     String str;
+     int n;
+     Scanner sj=new Scanner(System.in);
+     n=sj.nextInt();
+     str=sj.next();
+     for(int i=1;i<=n;i++){
+         System.out.println(str);
+     }
+    }
+}
