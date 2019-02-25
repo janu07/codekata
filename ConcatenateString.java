7fa720ff2625dee75a4ffa6441609bbc2f9c60a5
janu07 <47377091+janu07@users.noreply.github.com>
2019-02-25 08:52:51 -0800

    Create ConcatenateString.java

---
+++ ConcatenateString.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+class ConcatenateString
+{
+	public static void main(String[] args)
+	{
+		Scanner sc = new Scanner(System.in);
+		String firstStr = sc.next();
+		String secondStr = sc.next();
+		System.out.println(firstStr + secondStr);
+	}
+}
