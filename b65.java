5368c6713a224cef213203d7c4f144ee9a4ea073
janu07 <47377091+janu07@users.noreply.github.com>
2019-03-06 09:25:32 -0800

    Create b65.java

---
+++ b65.java
@@ -0,0 +1,10 @@
+import java.util.*;
+class b65
+{
+	public static void main(String args[])
+	{
+		Scanner sj=new Scanner(System.in);
+		String s=sj.nextLine();
+            System.out.print(s);
+	}
+}
