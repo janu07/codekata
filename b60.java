f1a739606459c7d834ba41e8b43d7d891524b09a
janu07 <47377091+janu07@users.noreply.github.com>
2019-03-06 08:18:20 -0800

    Create b60.java

---
+++ b60.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class b60
+{
+     public static void main(String aa[])
+{
+    int n,s=0;
+    Scanner sj=new Scanner(System.in);
+    n=sj.nextInt();
+    for(int i=1;i<=n;i++)
+        {
+            s=s+i;
+        }   
+System.out.println(s);
+		sj.close(); 
+}
+}
