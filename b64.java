d26fe50a415f0e9fde528fe641d5270ed762962b
janu07 <47377091+janu07@users.noreply.github.com>
2019-03-06 09:20:57 -0800

    Create b64.java

---
+++ b64.java
@@ -0,0 +1,22 @@
+import java.util.*;
+class b64
+{
+	public static void main(String args[])
+	{
+		Scanner sj=new Scanner(System.in);
+		int n1=sj.nextInt();
+		int n2=sj.nextInt();
+        int n=0;
+        n=n1+n2; 
+           if(n%2==0)
+           {
+            System.out.print("even");
+        }
+        else
+        {
+            System.out.print("odd");
+        }
+            
+       
+    }
+}
