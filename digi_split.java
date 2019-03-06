75bb5a3305443ce32ed869d28e90f1eeb5e5808a
janu07 <47377091+janu07@users.noreply.github.com>
2019-03-06 15:41:55 +0530

    Update digi_split.java

--- digi_split.java
+++ digi_split.java
@@ -1,17 +1,21 @@
 import java.util.*;
-class digi_split
+class Aver
 {
      public static void main(String aa[])
-    {
-      String a;
-      int n;
-      Scanner s=new Scanner(System.in);
-      n=s.nextInt();
-      a=String.valueOf(n);
-      char b[]=a.toCharArray();
-      for(int i=0;i<b.length;i++)
-      {
-         System.out.printf("%c\t",b[i]);
-      }
-    }
+{
+    String a;
+    int n;
+     Scanner s=new Scanner(System.in);
+     n=s.nextInt();
+     a=String.valueOf(n);
+     char b[]=a.toCharArray();
+     for(int i=0;i<b.length;i++)
+{
+         System.out.print(b[i]);
+if(i<b.length-1)
+{
+System.out.print(" ");
+}
+     } 
+}
 }
