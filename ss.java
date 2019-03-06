62d696465703cb6f5cd124a0a2f8d7d982b747d1
janu07 <47377091+janu07@users.noreply.github.com>
2019-03-06 07:13:53 -0800

    Create ss.java

---
+++ ss.java
@@ -0,0 +1,22 @@
+import java.util.*;
+class ss
+{
+     public static void main(String aa[])
+{
+    int n,n1,n2;
+     Scanner s=new Scanner(System.in);
+     n1=s.nextInt();
+	 n2=s.nextInt();
+	 n=n1*n2;
+	if(n%2==0) 
+{
+	System.out.println("Even");			
+}
+	else 
+{
+	System.out.println("Odd");	
+}
+
+		s.close(); 
+}
+}
