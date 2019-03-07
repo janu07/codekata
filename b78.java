4afa8b7c4bfb099b25f4313a678cc718c1e02c2c
janu07 <47377091+janu07@users.noreply.github.com>
2019-03-07 16:12:37 +0530

    Create b78.java

---
+++ b78.java
@@ -0,0 +1,22 @@
+import java.util.*;
+import java.lang.*;
+class b78
+{
+   public static void main(String args[])
+   {
+        Scanner sj = new Scanner(System.in);
+        int n= sj.nextInt();
+      
+           if(n%13==0)
+           {
+               System.out.print("yes");
+           }
+           else
+           {
+                System.out.print("no");
+           }
+      
+        
+                 
+   }
+} 
