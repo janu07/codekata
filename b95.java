44d8a7dd06f17ee1faf72899aea31404f79fbbea
janu07 <47377091+janu07@users.noreply.github.com>
2019-03-07 08:47:11 -0800

    Create b95.java

---
+++ b95.java
@@ -0,0 +1,15 @@
+mport java.util.*;
+class b95
+{
+	
+	public static void main(String[] args) 
+	{
+		int p,r,t;
+        Scanner	sj= new Scanner(System.in);
+        p=sj.nextInt();
+        t=sj.nextInt();
+        r=sj.nextInt();
+        System.out.print((r*t*p)/100);
+       
+	}
+}
