5fefdc3a32407b2a0e0440db1893897681ab01b7
janu07 <47377091+janu07@users.noreply.github.com>
2019-03-07 16:35:02 +0530

    Create b82.java

---
+++ b82.java
@@ -0,0 +1,16 @@
+import java.util.*;
+ class b82 
+{
+    public static void main(String args[])
+    {
+    Scanner sj=new Scanner(System.in);
+    float n1,n2;
+    double i; 
+	n1=sj.nextFloat(); 
+    n2=sj.nextFloat();
+
+	 i=n1*n2;
+     System.out.format("%.5f",i);
+    }
+ 
+}
