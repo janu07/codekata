1d3f8ea5d9c639981f9062a61c2ea966def2c65f
janu07 <47377091+janu07@users.noreply.github.com>
2019-03-07 16:44:41 +0530

    Create b84.java

---
+++ b84.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class b84
+{ 
+  
+    public static void main(String[] args) 
+    { 
+        Scanner sj=new Scanner(System.in);
+        char c =sj.next().charAt(0); 
+        int Aski = c; 
+        System.out.println(Aski); 
+    } 
+}
