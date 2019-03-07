9e88994667b90a221ee2c3c7b90dee84d3439940
janu07 <47377091+janu07@users.noreply.github.com>
2019-03-07 10:05:07 -0800

    Create b107.java

---
+++ b107.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+class b107
+{
+public static void main(String[] args) 
+{
+Scanner sj = new Scanner(System.in);
+ int a=sj.nextInt();
+ int b=sj.nextInt();
+ int c=sj.nextInt();
+System.out.print((a*b)/c);
+
+
+}
+}
