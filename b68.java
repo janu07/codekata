42aa69be3520804b87131b6045fe327c122a34cd
janu07 <47377091+janu07@users.noreply.github.com>
2019-03-07 15:15:26 +0530

    Create b68.java

---
+++ b68.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+class b68
+{
+    public static void main(String args[])
+    {
+        
+        Scanner sj = new Scanner(System.in);
+        int m = sj.nextInt();
+    
+       
+            if(m%7==0)
+            {
+                System.out.println("yes");
+            }
+            else
+            {
+                System.out.println("no");
+            }
+    }
+}
