adee26119f385bf926abe10b048480934ed07e38
janu07 <47377091+janu07@users.noreply.github.com>
2019-03-07 15:48:36 +0530

    Create b75.java

---
+++ b75.java
@@ -0,0 +1,30 @@
+import java.util.*;
+import java.lang.*;
+class b75
+{
+   public static void main(String args[])
+   {
+        String s;
+        Scanner sj = new Scanner(System.in);
+        str = sj.next();
+        int len=s.length();
+        char charArray[]=s.toCharArray();  
+        if(len%2!=0)
+        {
+        int mid=len/2;
+        charArray[mid]='*';
+        }
+        else
+        {
+        int mid=len/2;
+        charArray[mid-1]='*';
+        charArray[mid]='*';
+
+        }
+        for(int i=0;i<len;i++)
+        {            
+             System.out.print(charArray[i]);
+        }
+        
+   }
+} 
