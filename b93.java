07a4535d831895c9cb46fc0519ee9017448d9067
janu07 <47377091+janu07@users.noreply.github.com>
2019-03-07 08:25:20 -0800

    Create b93.java

---
+++ b93.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class b93
+{
+	
+	public static void main(String[] args) 
+	{
+		int l;
+        Scanner	sj= new Scanner(System.in);
+        l=sj.nextInt();
+        float kelvin= l+ 273.15f;
+        System.out.print(Math.round(kelvin));
+       
+	}
+}
