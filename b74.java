f85b20fb2ae51b118b868cc4c2389f9524d52001
janu07 <47377091+janu07@users.noreply.github.com>
2019-03-07 15:42:59 +0530

    Create b74.java

---
+++ b74.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.lang.*;
+class b74
+{
+   public static void main(String args[])
+   {
+        float num;
+        Scanner sj = new Scanner(System.in);
+        num = sj.nextFloat();
+         
+          System.out.println(Math.round(Math.ceil(num)));
+        
+   }
+} 
