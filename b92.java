b125a5974a6d14729f58014edf64d5ddecb86718
janu07 <47377091+janu07@users.noreply.github.com>
2019-03-07 08:19:27 -0800

    Create b92.java

---
+++ b92.java
@@ -0,0 +1,19 @@
+import java.util.*;
+class b92
+{
+	
+	public static void main(String[] args) 
+	{
+		int l,sum=0;
+        Scanner	sj = new Scanner(System.in);
+        l=sj.nextInt();
+        int arr[]= new int[l];
+        for(int i=0;i<l;i++)
+        {
+            arr[i]=sc.nextInt();
+            sum+=arr[i];
+        }
+        System.out.print(sum);
+       
+	}
+}
