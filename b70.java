3e1921487b7dbc3edf04676971e0bec169becbbc
janu07 <47377091+janu07@users.noreply.github.com>
2019-03-07 15:23:22 +0530

    Create b70.java

---
+++ b70.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class b70
+{
+    public static void main(String args[])
+    {
+        
+        Scanner sj = new Scanner(System.in);
+        int n = sj.nextInt();
+  System.out.println(n*2);
+    }
+}
