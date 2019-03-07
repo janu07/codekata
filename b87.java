3bbd0b8cedd88862d9fed5d0bb796c670f79f158
janu07 <47377091+janu07@users.noreply.github.com>
2019-03-07 07:41:36 -0800

    Create b87.java

---
+++ b87.java
@@ -0,0 +1,24 @@
+import java.lang.*;
+import java.util.*;
+ 
+ class b87
+{
+    public static void main(String args[])
+    {
+        int a,b,result=0;
+        Scanner sj=new Scanner(System.in);
+        a=sj.nextInt();
+        b=sj.nextInt();
+        
+        int n=Math.min(a,b);
+        for (int i=1;i<=n;i++)
+        {
+            if(a%i==0 && b%i==0)
+            {
+                result=i;
+            }
+        }
+      
+        System.out.println(result);
+    }
+}
