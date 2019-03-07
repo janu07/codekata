8b26e2820f5ca44354a31d75a3b607f3bea32767
janu07 <47377091+janu07@users.noreply.github.com>
2019-03-07 08:16:27 -0800

    Create b91.java

---
+++ b91.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class b91
+{
+	
+	public static void main(String[] args) 
+	{
+		int l,b,h;
+        Scanner	sj= new Scanner(System.in);
+        l=sj.nextInt();
+        b=sj.nextInt();
+        h=sj.nextInt();
+       System.out.print((2*l*b)+(2*l*h)+(2*h*b)+" ");
+        System.out.print(l*b*h);
+       
+	}
+}
