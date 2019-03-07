b85f997ae4d1d86e1f7f6d00ea6f39dcd2ff86f7
janu07 <47377091+janu07@users.noreply.github.com>
2019-03-07 09:50:17 -0800

    Create b105.java

---
+++ b105.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+class b105
+{
+public static void main(String[] args) 
+{
+Scanner sc=j = new Scanner(System.in);
+ int d=sj.nextInt();
+ int p=sj.nextInt();
+ System.out.print(d+""+p);
+
+}
+}
