789eea9990d0c8310383a6f7f98385fe1b6e6c09
janu07 <47377091+janu07@users.noreply.github.com>
2019-03-07 15:59:10 +0530

    Create b77.java

---
+++ b77.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.lang.*;
+class b77
+{
+   public static void main(String args[])
+   {
+        Scanner sj = new Scanner(System.in);
+        int n = sj.nextInt();
+       for(int i=1;i<=n;i++)
+       {
+           if(n%i==0)
+           {
+               System.out.print(i+" ");
+           }
+       }
+        
+            
+        
+   }
+} 
