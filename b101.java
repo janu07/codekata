247e57b7a9e5ba7ee4126105ec57b08e3640bb31
janu07 <47377091+janu07@users.noreply.github.com>
2019-03-07 09:30:26 -0800

    Create b101.java

---
+++ b101.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class b101
+{
+    public static void main(String args[])
+    {  
+        String s;
+        Scanner sj = new Scanner(System.in);
+        str = sj.next();
+        int n=sj.nextInt();
+        int len=s.length();
+        int find=len-n;
+        System.out.println(s.substring(find,len));
+    }
+}
