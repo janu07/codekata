0af9f96a8061592bc0b070a6722f17f41c7057c6
janu07 <47377091+janu07@users.noreply.github.com>
2019-03-08 11:53:38 +0530

    Create p4.java

---
+++ p4.java
@@ -0,0 +1,13 @@
+import java.util.*;
+import java.lang.*;
+ class p4
+{
+    public static void main(String args[])
+    {    
+        Scanner sj=new Scanner(System.in);
+        String  n=sj.next();
+        StringBuffer s=new StringBuffer(n);
+        String m=".";
+        System.out.println(s.append(m));
+    }
+}
