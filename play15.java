84cc6be066ce25d95932d81c4d4049ff4f87fd98
janu07 <47377091+janu07@users.noreply.github.com>
2019-03-10 18:01:25 +0530

    Create play15.java

---
+++ play15.java
@@ -0,0 +1,30 @@
+import java.util.*;
+  class play15 
+{ 
+      
+  
+    public static void main(String[] args) 
+    { 
+	Scanner input=new Scanner(System.in);
+        	String str = input.nextLine();
+        	int count[] = new int[256]; 
+        
+       	 int len = str.length(); 
+        	for (int i=0; i<len; i++) 
+	{	
+            		count[str.charAt(i)]++; 
+       	}
+        	int max = -1;   
+        	char result = ' ';    
+        
+        	for (int i = 0; i < len; i++) 
+	{ 
+            		if (max < count[str.charAt(i)]) 
+		{ 
+                			max = count[str.charAt(i)]; 
+                			result = str.charAt(i); 
+            		}	 
+        	}
+        	System.out.println(result); 
+    } 
+} 
