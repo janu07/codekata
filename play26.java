ff43d19ff317d79dc63d4c8a236e369d3c8dab57
janu07 <47377091+janu07@users.noreply.github.com>
2019-03-29 14:38:08 +0530

    Create play26.java

---
+++ play26.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class play26
+{
+    public static void main(String arg[])
+    {
+        Scanner input=new Scanner(System.in);
+        String str=input.nextLine();      
+        System.out.print(str.replaceAll("\\s+", " "));
+
+    }
+}
