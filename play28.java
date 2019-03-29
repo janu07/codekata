4266bab22afc6742bb9dd14ac4c08a894e43162b
janu07 <47377091+janu07@users.noreply.github.com>
2019-03-29 14:43:42 +0530

    Create play28.java

---
+++ play28.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class play28
+{
+    public static void main(String arg[])
+    {
+        Scanner sj=new Scanner(System.in);
+        String str=sj.nextLine();
+        
+        String string[]=str.split(" ");
+        for(int i=0;i<string.length;i++)
+        {
+        System.out.print(string[i]);
+        }
+
+    }
+}
